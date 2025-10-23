d8d487aae987acebe98c6ddb5f52899568cd6efa
FoxLoaves <99065699+BoredFurry@users.noreply.github.com>
2025-10-23 10:29:07 +0800

    Fix style again

--- src/main/java/seedu/address/logic/parser/AddressBookParser.java
+++ src/main/java/seedu/address/logic/parser/AddressBookParser.java
@@ -104,7 +104,7 @@ public class AddressBookParser {
 
         case UnmatchCommand.COMMAND_WORD:
             return new UnmatchCommandParser().parse(arguments);
-            
+
         default:
             logger.finer("This user input caused a ParseException: " + userInput);
             throw new ParseException(MESSAGE_UNKNOWN_COMMAND);
